599645400fbd1068e876a93080ea101b436436cb
mahadabdi9 <mahadabdi9@gmail.com>
2023-03-04 15:18:29 -0500

    cleaned up code and added docs folder with screenshots

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(function () {
-
-
   const loadTweets = function () {
     $.ajax("/tweets").then(function (response) {
       renderTweets(response);
@@ -15,14 +7,13 @@ $(document).ready(function () {
   loadTweets();
   $(".error").hide();
 
-
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     $("#tweets-container").empty();
     for (let tweet of tweets) {
       const tweetElement = createTweetElement(tweet);
-       $("#tweets-container").prepend(tweetElement);
+      $("#tweets-container").prepend(tweetElement);
     }
-  }
+  };
 
   const escape = function (str) {
     let div = document.createElement("div");
@@ -30,10 +21,10 @@ $(document).ready(function () {
     return div.innerHTML;
   };
 
-
-const createTweetElement = function(tweet) {
-    let $tweet = $("<article>").addClass("tweet");
-    const html = `
+  // creating new tweet
+  const createTweetElement = function (tweet) {
+    let $tweet = $("<article>").addClass("tweet");
+    const html = `
         <header>
           <div class="avatar-username">
           <img class="tweet--avatar" src="${tweet.user.avatars}"> 
@@ -44,7 +35,9 @@ const createTweetElement = function(tweet) {
         <div>
           <form action="">
             <label for=""></label>
-            <textarea name="" class="tweet-text" cols="30" rows="1">${escape(tweet.content.text)}</textarea>
+            <textarea name="" class="tweet-text" cols="30" rows="1">${escape(
+              tweet.content.text
+            )}</textarea>
           </form>
           <footer>
           ${timeago.format(tweet.created_at)}
@@ -55,44 +48,40 @@ const createTweetElement = function(tweet) {
             </div>
           </footer>
         </div>`;
-    const tweetElement = $tweet.append(html); 
-    return tweetElement;
+    const tweetElement = $tweet.append(html);
+    return tweetElement;
   };
 
-
-
-  $("#tweetForm").on("submit", function(event) {
+  // form submission
+  $("#tweetForm").on("submit", function (event) {
     event.preventDefault();
     const tweetArea = $(".tweet-text").val().trim().length;
     if (!tweetArea) {
       $(".error").text("Tweet Area cannot be empty");
-      $(".error").slideDown("slow")
+      $(".error").slideDown("slow");
       $(".error").delay(4000).slideUp("slow");
 
       return;
     }
     if (tweetArea > 140) {
       $(".error").text("Tweet cannot be more than 140 characters‼️");
-      $(".error").slideDown("slow")
+      $(".error").slideDown("slow");
       $(".error").delay(4000).slideUp("slow");
 
       return;
     }
     $.ajax({
-      url: '/tweets',
-      method: 'POST',
+      url: "/tweets",
+      method: "POST",
       data: $(this).serialize(),
-      success: function() {
+      success: function () {
         loadTweets();
-        $(".tweet-text").val("")
+        $(".tweet-text").val("");
+        $(".counter").text(140);
       },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.log(textStatus + ': ' + errorThrown);
-      }
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus + ": " + errorThrown);
+      },
     });
-
-
-})
-
-})
-
+  });
+});
